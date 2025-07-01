d196a8e9ee10b028c4c5126bd25de56ccb03c5fd
VinEckSie <new.riverside751@passmail.net>
2025-07-01 11:26:45 +0200

    test workflow

---
+++ target/debug/deps/sealed_in_rust-82047ec2434d6039.d
@@ -0,0 +1,5 @@
+/Users/vinecksie/RustroverProjects/sealed-in-rust/target/debug/deps/sealed_in_rust-82047ec2434d6039.d: src/main.rs
+
+/Users/vinecksie/RustroverProjects/sealed-in-rust/target/debug/deps/libsealed_in_rust-82047ec2434d6039.rmeta: src/main.rs
+
+src/main.rs:
