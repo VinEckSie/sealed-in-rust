6196caffaf8f097ebdd0c191ee0167e9b4d84fcf
VinEckSie <VinEckSie@users.noreply.github.com>
2026-03-27 07:18:13 +0000

    deploy: 1cbbd7d6da07ee465ba7bc28174754db95db29de

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c4d21d10.js',
+                path_to_root + 'searchindex-69594d0c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
