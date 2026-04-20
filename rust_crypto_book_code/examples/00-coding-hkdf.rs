cc5b51770332e2ef6c0b0ea0bd316afbbd110df8
VinEckSie <new.riverside751@passmail.net>
2026-04-20 18:31:37 +0200

    feat(fearless in rust): add code part for HKDF video

--- rust_crypto_book_code/examples/00-coding-hkdf.rs
+++ rust_crypto_book_code/examples/00-coding-hkdf.rs
@@ -1,1 +1,14 @@
-fn main() {}
+use hkdf::Hkdf;
+use sha2::Sha256;
+
+fn main() {
+    let salt: [u8; 16] = *b"unique-salt-1234";
+    let ikm: [u8; 32] = *b"0123456789ABCDEF0123456789ABCDEF";
+
+    let hk = Hkdf::<Sha256>::new(Some(&salt), &ikm);
+    let mut okm = [0u8; 32];
+
+    hk.expand(b"encryption key", &mut okm).unwrap();
+
+    println!("OKM: {:02x?}", okm);
+}
